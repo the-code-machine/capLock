9b94005fd8fbe52e4b4f6062c85476a1c657e5d8
the-code-machine <sarthak25ic049@satiengg.in>
2025-03-15 17:59:07 +0530

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -86,7 +86,29 @@ export default function RootLayout({ children }) {
             }),
           }}
         />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ID}`}></script>
+        <script
+          dangerouslySetInnerHTML={
+            {
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ID}',{
+            page_path: window.location.pathname,});
+            `,
+            }
+          }
+
+
+        />
+
+
       </Head>
+
+
       <body>
         <Toaster />
         <Navbar />
