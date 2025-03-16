9a289e11838ccb598d7c30bf6c011dc22186afba
Dev Jadiya <devjadiya15@gmail.com>
2025-03-16 12:31:50 +0530

    Page json

--- src/app/[...page]/page.js
+++ src/app/[...page]/page.js
@@ -23,7 +23,43 @@ export default function Page() {
     } else {
       document.title = "CapLock - 3D Printing & IoT Solutions";
     }
+    const jsonLd = {
+      "@context": "https://schema.org",
+      "@type": "Organization",
+      "name": "CapLock",
+      "url": "https://caplock.in",
+      "logo": "https://caplock.in/favicon.ico",
+      "description": "Discover premium 3D printing, IoT, and prototyping solutions at CapLock.",
+      "sameAs": [
+        "https://instagram.com/caplock.store",
+        "https://x.com/CaplockConnect",
+        "https://www.linkedin.com/in/caplock-store-0928b6356/"
+      ],
+      "address": {
+        "@type": "PostalAddress",
+        "addressLocality": "Vidisha",
+        "addressCountry": "India"
+      },
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+91-9752133459",
+        "contactType": "Customer Service",
+        "areaServed": "IN",
+        "availableLanguage": "English"
+      }
+    };
+
+    const script = document.createElement("script");
+    script.type = "application/ld+json";
+    script.innerHTML = JSON.stringify(jsonLd);
+    document.head.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.head.removeChild(script);
+    };
   }, [page]);
+
   return (
     <div>
       {page === "about" && <AboutPage />}
